509ccd368c66e4e7c75ae0d087c9c2d7c146f9e2
usr <usr@PC1>
2023-07-26 13:13:34 +0300

    Adds User controller
    Allows to Get an Authenticated User Info

---
+++ RetailManager_/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using RetailManager.Library.DataAccess;
+using System.Web.Http;
+using RetailManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+
+namespace RetailManager_.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public UserModel GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId).First();
+        }
+    }
+}
