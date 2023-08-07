fb4e33e8e850bd8a06148ac403e3a5ff98ca65c2
usr <usr@PC1>
2023-08-07 09:44:47 +0300

    Updates EF Datebase name
    Adds a db migration configuration file

---
+++ RetailManager_/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace RetailManager_.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RetailManager_.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(RetailManager_.Models.ApplicationDbContext context)
+        {
+            
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
